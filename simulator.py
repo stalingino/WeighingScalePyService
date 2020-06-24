35f16568532147fd7a336843c119335a1eb5b293
Stalin Gino <stalingino@gmail.com>
2020-06-24 11:59:10 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -8,7 +8,7 @@ class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.end_headers()
-        self.wfile.write(str.encode(str(round(random() * 10 + 10, 3))))
+        self.wfile.write(b'{"weight":"' + str.encode(str(round(random() * 10 + 10, 3))) + b'"}')
 
 httpd = socketserver.TCPServer(('', 3303), Handler)
 httpd.serve_forever()
