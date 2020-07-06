a7b1cddc85c48977e6137c717a0af4ed40a3f727
Stalin Gino <stalingino@gmail.com>
2020-07-06 22:02:33 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -7,11 +7,15 @@ from random import random
 class Handler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
+        self.send_header("Cache-Control", "no-cache")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET,OPTIONS')
         self.end_headers()
         self.wfile.write(b'{"weight":"' + str.encode(str(round(random() * 10 + 10, 3))) + b'"}')
 
     def do_OPTIONS(self):
         self.send_response(HTTPStatus.OK)
+        self.send_header("Cache-Control", "no-cache")
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET,OPTIONS')
         self.end_headers()
