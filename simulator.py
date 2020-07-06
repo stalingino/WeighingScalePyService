afdf4da0d52766a1eb0a1c20739f036d5f7ed1ea
Stalin Gino <stalingino@gmail.com>
2020-07-06 22:36:46 +0530

    Update simulator.py

--- simulator.py
+++ simulator.py
@@ -5,9 +5,6 @@ from http import HTTPStatus
 from random import random
 
 class Handler(http.server.SimpleHTTPRequestHandler):
-    def __init__(self, request, client_address, server):
-        self.weight = None
-    
     def do_GET(self):
         self.send_response(HTTPStatus.OK)
         self.send_header("Cache-Control", "no-cache")
@@ -17,14 +14,14 @@ class Handler(http.server.SimpleHTTPRequestHandler):
         self.end_headers()
         print(self.path)
         if self.path == '/getWeight':
-            if self.weight:
-                w = self.weight
-                self.weight = None
+            if hasattr(self, 'weight'):
+                w = getattr(self, 'weight')
+                delattr(self, 'weight')
             else:
                 w = str(round(random() * 10 + 10, 3))
             self.wfile.write(b'{"weight":"' + str.encode(w) + b'"}')
         elif self.path.startswith('/setWeight?weight='):
-            self.weight = self.path.split('=')[1]
+            setattr(self, 'weight', self.path.split('=')[1])
 
     def do_OPTIONS(self):
         self.send_response(HTTPStatus.OK)
